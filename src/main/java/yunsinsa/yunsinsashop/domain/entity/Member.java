f309d53747de44da86860de4de52eb926f3df983
JE\yunje <dev.byje@gmail.com>
2023-10-28 20:56:41 +0900

    1028_1

--- src/main/java/yunsinsa/yunsinsashop/domain/entity/Member.java
+++ src/main/java/yunsinsa/yunsinsashop/domain/entity/Member.java
@@ -4,6 +4,7 @@ package yunsinsa.yunsinsashop.domain.entity;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Setter
 @Entity
@@ -11,16 +12,29 @@ import javax.persistence.*;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // id 생성을 db 로 위임
-    @Column(name = "member_id", columnDefinition = "일련번호")
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // id 생성을 db 로 위임 :자동 증가(primary key) 역할
+    @Column(name = "member_id", columnDefinition = "INT")
     private Long id;
 
-    @Column(name = "member_name", columnDefinition = "회원 이름")
+    @Column(name = "member_name", length = 255, nullable = false)
     private String name;
 
-    @Column(name = "email", columnDefinition =  "회원 이메일")
+    @Column(name = "email", length = 255, nullable = false)
     private String email;
 
-    @Column(name="password", columnDefinition = "비밀번호")
+    @Column(name="password", columnDefinition = "PASSWORD")
     private String password;
+
+    @Column(name="created_at", columnDefinition ="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp createdAt;
+
+    @Column(name="updated_at", columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private Timestamp updateAt;
+
+    @Column(name="created_by", columnDefinition="INT")
+    private int createdBy;
+
+    @Column(name="updated_by", columnDefinition="INT")
+    private int updatedBy;
+
 }
