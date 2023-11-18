eaf7bd18937399e359692a06fc8def2aa4faee17
JE\yunje <dev.byje@gmail.com>
2023-11-18 23:23:40 +0900

    11/18 member 컨트롤러 수정-완료

--- src/main/java/yunsinsa/yunsinsashop/presentation/controller/MemberController.java
+++ src/main/java/yunsinsa/yunsinsashop/presentation/controller/MemberController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 import yunsinsa.yunsinsashop.domain.service.MemberService;
 import yunsinsa.yunsinsashop.presentation.dto.MemberDto;
 
+import java.util.List;
+
 @RequiredArgsConstructor //final 으로 쓰인 대상자에 관해서 생성자를 만든다
 @RestController
 @RequestMapping("/members")
@@ -17,16 +19,27 @@ public class MemberController {
     public MemberDto.CreateResponse createMember(@RequestBody MemberDto.CreateRequest request){
      return memberService.createMember(request);
     }
+
+
 //회원 조회
     @GetMapping
     public MemberDto.FindResponse findResponse(@PathVariable Long id){
         return memberService.findMember(id);
     }
 
+
+// 모든 회원 조회
+    @GetMapping
+    public List<MemberDto.FindResponse> findResponses(){
+        return memberService.findAllMembers();
+    }
+
+
 //회원 정보수정
     @PutMapping
     public void updateMember(@RequestBody MemberDto.UpdateRequest request){
     memberService.updateMember(request); }
+
 
 //회원 삭제
     @DeleteMapping
